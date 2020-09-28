71b6c6ab92cb163f5505ebdd0e8ae0d81cdcc971
Humaira <humaira.nu@gmail.com>
2020-09-28 14:21:10 +0200

    Model Classes added
    Model Classes added  to read the data into Application
    -Entity classes are created
    -Enum arrays are used to assign the weight to each attribute
    -Defination of CalculateEntityWeight function is added

---
+++ LsmSearchApp/LsmSearchApp/Models/EntitiesData.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LsmSearchApp.Models
+{
+    public class EntitiesData
+    { // List of entities which are present inside the JSON file- same names for the list are used
+        public List<Building> buildings { get; set; }
+        public List<Lock> locks { get; set; }
+        public List<Group> groups { get; set; }
+        public List<Medium> media { get; set; }
+    }
+}
---
+++ LsmSearchApp/LsmSearchApp/Models/Lock.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LsmSearchApp.Models
+{
+    // Weights of Lock Attributes
+    public enum LockWeight
+    {
+        // Weight of Own Attributes
+        TypeWeight = 3,
+        NameWeight = 10,
+        SerialNumberWeight = 8,
+        FloorWeight = 6,
+        RoomNumberWeight = 6,
+        DescriptionWeight = 6,
+    }
+
+
+    // this class represents the Lock entity of the Data
+    public class Lock : Entity
+    {
+        // Attributes of Group
+        public string BuildingId { get; set; }
+        public Building Building { get; set; }
+        public string Type { get; set; }
+        public string Name { get; set; }
+        public string SerialNumber { get; set; }
+        public string Floor { get; set; }
+        public string RoomNumber { get; set; }
+        public string Description { get; set; }
+
+        public Lock()
+        {
+        }
+
+
+        // Calculate the weight of Lock record based on searchText
+        public void CalculateEntityWeight(string searchText)
+        {
+        }
+
+     }
+}
